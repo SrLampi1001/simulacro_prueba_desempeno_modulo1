93157243f179555f17fd1dcd11314cb98f7aa781
Santiago S <sanchezsantiago1001@gmail.com>
2025-11-26 21:13:12 -0500

    usuarios archive docstrings added

--- usuarios.py
+++ usuarios.py
@@ -9,6 +9,9 @@ class User:
     equipment_in_posesion : list[Equipment] #Shoul be a list of Equipments
     
     def __init__(self, user, password, rol, email): #The constructor MUST NOT be called directly to create users
+        """
+        Constructor
+        """
         self.user = user
         self.password = password #Encrypts password
         self.email = email
@@ -16,6 +19,12 @@ class User:
     
     @classmethod
     def create(cls, user, password, rol, email = ""):
+        """
+        Class method for creation of users
+        Use instead of normal constructor
+        Makes verifications, allow user creation if passes all verifications
+        Else returns None
+        """
         if user in cls.__users: #usernames must be unique (As there no ID)
             print("UserName not avaible, try another")
             return None
@@ -51,6 +60,13 @@ class User:
         return encrypted
     @classmethod
     def login(cls, user, password, tr = 1):
+        """
+        Classmethod to login as an user
+        Verifies if user exist on memory
+        returns tuple user, true if login succesfull
+        if login unsuccesfull makes a callback to itself, up to 3 times
+        if user does not exist or the 3 times are up, returns tuple None, False
+        """
         encripted = cls.encryptPassword(password)
         if tr == 4:
             return None, False
@@ -65,6 +81,11 @@ class User:
             return None, False
         
     def save_user(self): 
+        """
+        Only saves the user on the csv archive
+        UPDATE EXPECTED:
+        verifies userName and updates the specific row on csv (Prevents duplicate users)
+        """
         dir  = os.path.dirname(self.users_path)
         if not os.path.exists(dir):
             self.create_directory(dir)
@@ -74,23 +95,40 @@ class User:
     
     def isAdmin(self):
         """
+        Simple method to verify admin status
         """
         if self.rol == "admin":
             return True
         return False
     
     def get_user(self):
+        """
+        user getter
+        returns list with all user parameters
+        """
         return [self.user, self.password, self.rol, self.email]
     
     def get_rol(self):
+        """
+        rol getter
+        returns user's rol
+        """
         return self.rol
     
     def get_email(self):
+        """
+        email getter
+        returns user's email
+        """
         if self.email=="":
             return None
         return self.email
     
-    def create_directory(self, dir):        
+    def create_directory(self, dir):
+        """
+        Creates the directory for the csv storing
+        Creates the csv archive on the path and creates the header row
+        """
         try:
             os.mkdir(dir)
             with open(self.users_path, "w") as users:
@@ -99,7 +137,12 @@ class User:
         except Exception as ex:
             print(f"Error, exception: {ex} creating the {dir} directory to save users")
         
-    def return_equipment(self, name, status)->bool:
+    def return_equipment(self, equipment_id, status)->bool:
+        """
+        receives status parameter from the user (Damaged or invalid)
+        Receives equipment id to return
+        Return True if equipment is returned, else False
+        """
         valid_status = ["available", "damaged"]
         if status not in valid_status:
             print(f"Error, {status} is not a valid status to return a equipment, only available or damaged")
@@ -107,7 +150,7 @@ class User:
         if len(self.equipment_in_posesion)==0:
             return False
         for equipment in self.equipment_in_posesion:
-            if equipment.name == name:
+            if equipment.id == equipment_id:
                 equipment.return_equipment()
                 return True
         return False
